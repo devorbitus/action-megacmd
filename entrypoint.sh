bf538a715180e48c5d65a646c543ed3ec9e56881
Chris Gruel <cgruel@gmail.com>
2021-08-17 10:36:16 -0400

    latest debian mega

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-bash -c  "nohup tail -f /root/.megaCmd/megacmdserver.log >&0 2>1" &
+bash -c "nohup tail -f /root/.megaCmd/megacmdserver.log >&0 2>1" &
 if [[ "$USERNAME" != "NOBODY" ]] && [[ "$PASSWORD" != "CHANGEME" ]]; then
     mega-login $USERNAME $PASSWORD
 fi
 
 if [[ "$*" == "" ]]; then
-	echo "Please specify a valid megacli command"
-    	exit 1
+    echo "Please specify a valid megacli command"
+    exit 1
 fi
 
 sh -c "mega-$*"
